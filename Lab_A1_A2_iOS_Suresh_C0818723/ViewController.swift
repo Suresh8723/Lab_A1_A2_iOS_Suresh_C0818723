86d9a4a12f7f48779714b5392c9eff0540a7788c
Suresh <C0818723@mylambton.ca>
2021-09-21 19:48:49 -0400

    Initial Commit

---
+++ Lab_A1_A2_iOS_Suresh_C0818723/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Suresh_C0818723
+//
+//  Created by Mac on 2021-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
